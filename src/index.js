df05c22a00c5e0f292b6737bba5f6e8aa56499ae
christina03 <m18890368211@163.com>
2019-10-08 15:43:40 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 'use strict';
 
-let has = Object.prototype.hasOwnProperty;
 let strUrl = 'http://192.168.0.101:8880/example/test.html?type=12345&name=zhangxia';
 
 function parseToStr(url){
-    const index = url.indexOf("?");
     let str = '';
-    if (index != -1) {
-        str = url.substr(index+1);
+    if(url){
+        const index = url.indexOf("?");
+        if (index != -1) {
+            str = url.substr(index+1);
+        }
     }
+    
     return str;
 }
 
@@ -16,14 +18,17 @@ console.log(parseToStr(strUrl))
 
 function parseToJson(url){
     let theRequest = new Object();
-    const index = url.indexOf("?");
-    if (index != -1) {
-        let str = url.substr(index+1);
-        let strs = str.split("&");
-        for(let i = 0; i < strs.length; i ++) {
-            theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);
+    if(url){
+        const index = url.indexOf("?");
+        if (index != -1) {
+            let str = url.substr(index+1);
+            let strs = str.split("&");
+            for(let i = 0; i < strs.length; i ++) {
+                theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);
+            }
         }
     }
+    
     return theRequest;
 }
 
